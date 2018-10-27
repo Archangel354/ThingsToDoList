01f34f2f2a713b1085d8a5604a93dee6be54e630
Archangel354 <rduncan354@gmail.com>
2018-10-27 17:49:50 -0400

    Added several files  AppExecutors and MainViewmodel.java

--- app/src/main/java/com/example/e244194/thingstodolist/MainActivity.java
+++ app/src/main/java/com/example/e244194/thingstodolist/MainActivity.java
@@ -1,12 +1,23 @@
 package com.example.e244194.thingstodolist;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.util.Log;
+import android.view.View;
 
 import com.example.e244194.thingstodolist.database.ItemDatabase;
+import com.example.e244194.thingstodolist.database.ItemEntry;
+
+import java.util.List;
 
 import static android.support.v7.widget.DividerItemDecoration.VERTICAL;
 
@@ -38,10 +49,69 @@ public class MainActivity extends AppCompatActivity implements ItemAdapter.ItemC
 
         DividerItemDecoration decoration = new DividerItemDecoration(getApplicationContext(), VERTICAL);
         mRecyclerView.addItemDecoration(decoration);
+
+        /*
+         Add a touch helper to the RecyclerView to recognize when a user swipes to delete an item.
+         An ItemTouchHelper enables touch behavior (like swipe and move) on each ViewHolder,
+         and uses callbacks to signal when a user is performing these actions.
+         */
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            // Called when a user swipes left or right on a ViewHolder
+            @Override
+            public void onSwiped(final RecyclerView.ViewHolder viewHolder, int swipeDir) {
+                // Here is where you'll implement swipe to delete
+                AppExecutors.getInstance().diskIO().execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        int position = viewHolder.getAdapterPosition();
+                        List<ItemEntry> tasks = mAdapter.getEntries();
+                        mDb.taskDao().deleteItem(tasks.get(position));
+                    }
+                });
+            }
+        }).attachToRecyclerView(mRecyclerView);
+
+        /*
+         Set the Floating Action Button (FAB) to its corresponding View.
+         Attach an OnClickListener to it, so that when it's clicked, a new intent will be created
+         to launch the AddTaskActivity.
+         */
+        FloatingActionButton fabButton = findViewById(R.id.fab);
+
+        fabButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to start an AddTaskActivity
+                Intent addTaskIntent = new Intent(MainActivity.this, AddItemActivity.class);
+                startActivity(addTaskIntent);
+            }
+        });
+
+        mDb = ItemDatabase.getInstance(getApplicationContext());
+        setupViewModel();
+    }
+
+    private void setupViewModel() {
+        MainViewModel viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+        viewModel.getItems().observe(this, new Observer<List<ItemEntry>>() {
+            @Override
+            public void onChanged(@Nullable List<ItemEntry> itemEntries) {
+                Log.d(TAG, "Updating list of items from LiveData in ViewModel");
+                mAdapter.setItems(itemEntries);
+            }
+        });
     }
 
     @Override
     public void onItemClickListener(int itemId) {
-
+        // Launch AddTaskActivity adding the itemId as an extra in the intent
+        Intent intent = new Intent(MainActivity.this, AddItemActivity.class);
+        intent.putExtra(AddItemActivity.EXTRA_TASK_ID, itemId);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/e244194/thingstodolist/MainViewModel.java
@@ -0,0 +1,33 @@
+package com.example.e244194.thingstodolist;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.example.e244194.thingstodolist.database.ItemDatabase;
+import com.example.e244194.thingstodolist.database.ItemEntry;
+
+import java.util.List;
+
+public class MainViewModel extends AndroidViewModel {
+
+    // Constant for logging
+    private static final String TAG = MainViewModel.class.getSimpleName();
+
+    private LiveData<List<ItemEntry>> items;
+
+
+    public MainViewModel(Application application) {
+        super(application);
+        ItemDatabase database = ItemDatabase.getInstance(this.getApplication());
+        Log.d(TAG, "Actively retrieving the items from the DataBase");
+        items = database.taskDao().loadAllItems();
+    }
+
+    public LiveData<List<ItemEntry>> getItems() {
+        return items;
+    }
+
+}
