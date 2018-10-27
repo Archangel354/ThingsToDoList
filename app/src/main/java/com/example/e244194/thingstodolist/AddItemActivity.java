e642262c2a57eecadc13917301a32153ce43f779
Archangel354 <rduncan354@gmail.com>
2018-10-27 12:42:07 -0400

    Created activity_add_item.xml, strings.xml and, colors.xml, dimens.xml and styles.xml.   I need to add data to the Room database

--- app/src/main/java/com/example/e244194/thingstodolist/AddItemActivity.java
+++ app/src/main/java/com/example/e244194/thingstodolist/AddItemActivity.java
@@ -2,8 +2,35 @@ package com.example.e244194.thingstodolist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+
+import com.example.e244194.thingstodolist.database.ItemDatabase;
 
 public class AddItemActivity extends AppCompatActivity {
+
+    // Extra for the task ID to be received in the intent
+    public static final String EXTRA_TASK_ID = "extraTaskId";
+    // Extra for the task ID to be received after rotation
+    public static final String INSTANCE_TASK_ID = "instanceTaskId";
+    // Constants for priority
+    public static final int PRIORITY_HIGH = 1;
+    public static final int PRIORITY_MEDIUM = 2;
+    public static final int PRIORITY_LOW = 3;
+    // Constant for default task id to be used when not in update mode
+    private static final int DEFAULT_TASK_ID = -1;
+    // Constant for logging
+    private static final String TAG = AddItemActivity.class.getSimpleName();
+    // Fields for views
+    EditText mEditText;
+    RadioGroup mRadioGroup;
+    Button mButton;
+
+    private int mTaskId = DEFAULT_TASK_ID;
+
+    // Member variable for the Database
+    private ItemDatabase mDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/e244194/thingstodolist/database/ItemDao.java
+++ app/src/main/java/com/example/e244194/thingstodolist/database/ItemDao.java
@@ -1,6 +1,7 @@
 package com.example.e244194.thingstodolist.database;
 
 import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.OnConflictStrategy;
@@ -9,6 +10,7 @@ import android.arch.persistence.room.Update;
 
 import java.util.List;
 
+@Dao
 public interface ItemDao {
 
     @Query("SELECT * FROM items ORDER BY priority")
