54d588ab0bc7673ea628023465fe71dc17ac7801
Archangel354 <rduncan354@gmail.com>
2018-10-27 16:40:34 -0400

    got mainactivity adapter instantian to finally work by selecting the 3rd recommended fix by the red lightbulb

--- app/src/main/java/com/example/e244194/thingstodolist/ItemAdapter.java
+++ app/src/main/java/com/example/e244194/thingstodolist/ItemAdapter.java
@@ -1,6 +1,14 @@
 package com.example.e244194.thingstodolist;
 
 import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.e244194.thingstodolist.database.ItemEntry;
 
@@ -8,10 +16,10 @@ import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.Locale;
 
-public class ItemAdapter {
+public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder> {
 
     // Constant for date format
-    private static final String DATE_FORMAT = "dd/MM/yyy";
+    private static final String DATE_FORMAT = "MM/dd/yyy";
 
     // Member variable to handle item clicks
     final private ItemClickListener mItemClickListener;
@@ -22,12 +30,133 @@ public class ItemAdapter {
     private SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT, Locale.getDefault());
 
 
-    public ItemAdapter(ItemClickListener listener, Context context) {
+    /**
+     * Constructor for the TaskAdapter that initializes the Context.
+     *
+     * @param context  the current Context
+     * @param listener the ItemClickListener
+     */
+    public ItemAdapter(Context context, ItemClickListener listener) {
         mItemClickListener = listener;
         mContext = context;
     }
 
+    @NonNull
+    @Override
+    public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // Inflate the task_layout to a view
+        View view = LayoutInflater.from(mContext)
+                .inflate(R.layout.item_layout, parent, false);
+
+        return new ItemViewHolder(view);
+    }
+
+    /**
+     * Called by the RecyclerView to display data at a specified position in the Cursor.
+     *
+     * @param holder   The ViewHolder to bind Cursor data to
+     * @param position The position of the data in the Cursor
+     */
+    @Override
+    public void onBindViewHolder(ItemViewHolder holder, int position) {
+        // Determine the values of the wanted data
+        ItemEntry itemEntry = mItemEntries.get(position);
+        String description = itemEntry.getDescription();
+        int priority = itemEntry.getPriority();
+        String updatedAt = dateFormat.format(itemEntry.getUpdatedAt());
+
+        //Set values
+        holder.itemDescriptionView.setText(description);
+        holder.updatedAtView.setText(updatedAt);
+
+        // Programmatically set the text and color for the priority TextView
+        String priorityString = "" + priority; // converts int to String
+        holder.priorityView.setText(priorityString);
+
+        GradientDrawable priorityCircle = (GradientDrawable) holder.priorityView.getBackground();
+        // Get the appropriate background color based on the priority
+        int priorityColor = getPriorityColor(priority);
+        priorityCircle.setColor(priorityColor);
+
+    }
+
+    /*
+    Helper method for selecting the correct priority circle color.
+    P1 = red, P2 = orange, P3 = yellow
+    */
+    private int getPriorityColor(int priority) {
+        int priorityColor = 0;
+
+        switch (priority) {
+            case 1:
+                priorityColor = ContextCompat.getColor(mContext, R.color.materialRed);
+                break;
+            case 2:
+                priorityColor = ContextCompat.getColor(mContext, R.color.materialOrange);
+                break;
+            case 3:
+                priorityColor = ContextCompat.getColor(mContext, R.color.materialYellow);
+                break;
+            default:
+                break;
+        }
+        return priorityColor;
+    }
+    /**
+     * Returns the number of items to display.
+     */
+    @Override
+    public int getItemCount() {
+        if (mItemEntries == null) {
+            return 0;
+        }
+        return mItemEntries.size();
+    }
+
+    public List<ItemEntry> getEntries() {
+        return mItemEntries;
+    }
+
+    /**
+     * When data changes, this method updates the list of taskEntries
+     * and notifies the adapter to use the new values on it
+     */
+    public void setItems(List<ItemEntry> ItemEntries) {
+        mItemEntries = ItemEntries;
+        notifyDataSetChanged();
+    }
+
     public interface ItemClickListener {
         void onItemClickListener(int itemId);
+    }
+
+
+    class ItemViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        // Class variables for the task description and priority TextViews
+        TextView itemDescriptionView;
+        TextView updatedAtView;
+        TextView priorityView;
+
+        /**
+         * Constructor for the TaskViewHolders.
+         *
+         * @param itemView The view inflated in onCreateViewHolder
+         */
+        public ItemViewHolder(View itemView) {
+            super(itemView);
+
+            itemDescriptionView = itemView.findViewById(R.id.taskDescription);
+            updatedAtView = itemView.findViewById(R.id.taskUpdatedAt);
+            priorityView = itemView.findViewById(R.id.priorityTextView);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            int elementId = mItemEntries.get(getAdapterPosition()).getId();
+            mItemClickListener.onItemClickListener(elementId);
+
+        }
     }
 }
--- app/src/main/java/com/example/e244194/thingstodolist/MainActivity.java
+++ app/src/main/java/com/example/e244194/thingstodolist/MainActivity.java
@@ -1,13 +1,47 @@
 package com.example.e244194.thingstodolist;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.e244194.thingstodolist.database.ItemDatabase;
+
+import static android.support.v7.widget.DividerItemDecoration.VERTICAL;
+
+public class MainActivity extends AppCompatActivity implements ItemAdapter.ItemClickListener{
+
+    // Constant for logging
+    private static final String TAG = MainActivity.class.getSimpleName();
+    // Member variables for the adapter and RecyclerView
+    private RecyclerView mRecyclerView;
+    private ItemAdapter mAdapter;
+
+    private ItemDatabase mDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set the RecyclerView to its corresponding view
+        mRecyclerView = findViewById(R.id.RecyclerViewItems);
+
+        // Set the layout for the RecyclerView to be a linear layout, which measures and
+        // positions items within a RecyclerView into a linear list
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Initialize the adapter and attach it to the RecyclerView
+      mAdapter = new ItemAdapter(MainActivity.this, this);
+      mRecyclerView.setAdapter(mAdapter);
+
+        DividerItemDecoration decoration = new DividerItemDecoration(getApplicationContext(), VERTICAL);
+        mRecyclerView.addItemDecoration(decoration);
+    }
+
+    @Override
+    public void onItemClickListener(int itemId) {
+
     }
 }
